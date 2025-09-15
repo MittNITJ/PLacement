1568e3220fff8b673e1baae199bcad30870f2d77
mittkumar <130504765+MittNITJ@users.noreply.github.com>
2025-09-15 18:25:00 +0000

    Comparison

---
+++ 01_basics/04_comparator.js
@@ -0,0 +1,10 @@
+console.log(2>1);
+console.log(2>=1); // answerin boolean 
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);// it will give true
+// in case of taking undefined in all three cases it gives false
+// === strictly checks even datatype also
+console.log("2"==2)
+console.log("2"===2)
