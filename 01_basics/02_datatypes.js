36971d5a3685ccae3edbae722f28d028de933f87
mittkumar <130504765+MittNITJ@users.noreply.github.com>
2025-09-15 16:47:45 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all JS code as newer version
+let name="mitt"
+let age=18
+let isLoggedIn=false
+let state;
+// alert(3+3)  //we are using nodejs,not browser
+console.log(3+3)
+console.log("Hitesh")
+// 2documentation tc39 and MDN
+//Primitive datatypes 
+//number=>2 to power 53 range 
+//bigint for large range 
+//string=>""
+//boolean=>true/false
+//null=>standalone value 
+//undefined=>
+//symbol=>unique
+console.log(typeof age)
+console.log(typeof undefined); // undefined type
+console.log(typeof null);  //type is object
