7bbfed587a8d293b0b12ae9e5b4f3c0f436cf6f2
mittkumar <130504765+MittNITJ@users.noreply.github.com>
2025-09-15 16:47:58 +0000

    variable

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,10 @@
+const accountId=14453
+let accountEmail="hitesh@google.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+//accountId=2
+/*
+prefer not to use Var
+*/
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
